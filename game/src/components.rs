01882c05921f835f307dc06d26690b5b76c0ec96
Guilhem Jazeron <guilhem.jazeron@telecom-paris.fr>
2022-06-22 13:40:07 +0200

    affichage du joueur sur le background

--- game/src/components.rs
+++ game/src/components.rs
@@ -1,11 +1,32 @@
 use bevy::prelude::*;
 
+
+// region : general components
+
 #[derive(Component)]
-pub enum Direction{
-    Up,
-    Left,
-    Right,
+pub enum Movement{
+    Idle,
+    Running,
+}
+
+#[derive(Component)]
+pub enum Direction {
+	
 }
 
 #[derive(Component, Deref, DerefMut)]
 pub struct AnimationTimer(pub Timer);
+//endregion : general components 
+
+//region : player components
+
+#[derive(Component)]
+pub struct PlayerComponent;
+
+//endregion : player components
+
+//region : enemy components
+#[derive(Component)]
+pub struct EnemyComponent;
+
+//endregion : enemy components 
--- game/src/main.rs
+++ game/src/main.rs
@@ -1,50 +1,61 @@
 mod components;
+mod player;
 
 use components::*;
 use bevy::{prelude::*, render::texture::ImageSettings};
 
+const BACKGROUND : &str = "textures/forest/Free Pixel Art Forest/Preview/Background.png";
+const CROUCH_SPRITE : &str = "textures/knight/Colour1/NoOutline/120x80_PNGSheets/_Crouch.png";
+
+struct WinSize{
+    win_h : f32,
+    win_w : f32,
+}
+
+struct GroundLevel(f32);
 
 fn main() {
     App::new()
-        .insert_resource(ImageSettings::default_nearest()) // prevents blurry sprites
+        .insert_resource(WindowDescriptor {
+            title: "ProjetX".to_string(),
+            width: 928.0,
+            height: 793.0,
+            resizable: false,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(animate_sprite)
         .run();
 }
 
-fn animate_sprite(
-    time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        &Handle<TextureAtlas>,
-    )>,
-) {
-    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
-        }
-    }
-}
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let texture_handle = asset_server.load("textures/knight/Colour1/Outline/120x80_PNGSheets/_Run.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(120.0, 80.0), 10, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+fn setup(mut commands : Commands, asset_server : Res<AssetServer>, windows : Res<Windows>){
     commands.spawn_bundle(Camera2dBundle::default());
-    commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(6.0)),
-            ..default()
-        })
-        .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
+    let background_image: Handle<Image> = asset_server.load(BACKGROUND);
+    commands.spawn_bundle(SpriteBundle {
+        texture : background_image,
+        transform : Transform {
+            translation : Vec3::new(0.,0.,0.),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+    if let Some(window) = windows.get_primary() {
+        let (win_h, win_w) = (window.height(),window.width());
+        let ground_lvl :f32 = -win_h/2. + 103.;
+        commands.insert_resource(WinSize {win_h, win_w,});
+        commands.insert_resource(GroundLevel(ground_lvl));
+        let player_sprite = asset_server.load(CROUCH_SPRITE);
+        commands.spawn_bundle(SpriteBundle{
+            texture : player_sprite,
+            transform : Transform {
+                translation : Vec3::new(0.,ground_lvl,1.),
+                //scale : Vec3::new(2.,2.,1.),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
     }
+    
+    
+}
