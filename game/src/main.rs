8409a89d5d26f0de02ee6817bdc6251980a37f46
Guilhem Jazeron <guilhem.jazeron@telecom-paris.fr>
2022-06-22 15:20:00 +0200

    Le chevalier court sur le fond, il reste à le faire se déplacer avec les flèches

--- game/src/main.rs
+++ game/src/main.rs
@@ -3,6 +3,7 @@ mod player;
 
 use components::*;
 use bevy::{prelude::*, render::texture::ImageSettings};
+use player::PlayerPlugin;
 
 const BACKGROUND : &str = "textures/forest/Free Pixel Art Forest/Preview/Background.png";
 const CROUCH_SPRITE : &str = "textures/knight/Colour1/NoOutline/120x80_PNGSheets/_Crouch.png";
@@ -16,6 +17,7 @@ struct GroundLevel(f32);
 
 fn main() {
     App::new()
+        .insert_resource(ImageSettings::default_nearest()) //prevent blurry sprites
         .insert_resource(WindowDescriptor {
             title: "ProjetX".to_string(),
             width: 928.0,
@@ -24,7 +26,9 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup)
+        .add_system(animate_sprite)
         .run();
 }
 
@@ -45,7 +49,7 @@ fn setup(mut commands : Commands, asset_server : Res<AssetServer>, windows : Res
         let ground_lvl :f32 = -win_h/2. + 103.;
         commands.insert_resource(WinSize {win_h, win_w,});
         commands.insert_resource(GroundLevel(ground_lvl));
-        let player_sprite = asset_server.load(CROUCH_SPRITE);
+        /* let player_sprite = asset_server.load(CROUCH_SPRITE);
         commands.spawn_bundle(SpriteBundle{
             texture : player_sprite,
             transform : Transform {
@@ -54,8 +58,24 @@ fn setup(mut commands : Commands, asset_server : Res<AssetServer>, windows : Res
                 ..Default::default()
             },
             ..Default::default()
-        });
+        }); */
+    }   
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+        &Handle<TextureAtlas>,
+    )>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
     }
-    
-    
 }
--- game/src/player.rs
+++ game/src/player.rs
@@ -1,31 +1,42 @@
 use bevy::prelude::*;
 use crate::{components::*, GroundLevel};
 
-const CROUCH_SPRITE : &str = "textures/knight/Colour1/NoOutline/120x80_PNGSheets/_Crouch.png";
-/* 
-struct PlayerPlugin;
+const RUN_SPRITE : &str = "textures/knight/Colour1/NoOutline/120x80_PNGSheets/_Run.png";
+const IDLE_SPRITE : &str = "textures/knight/Colour1/NoOutline/120x80_PNGSheets/_Idle.png";
+pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app : &mut App){
-        app.add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(0.5))
-                .with_system(player_spawn_system),
-        );
+        app.add_startup_system_to_stage(StartupStage::PostStartup, player_setup);
     }
     
 }
 
-fn player_spawn_system(mut commands : Commands, asset_server : AssetServer, ground_level : Res<GroundLevel>){
-    let player_sprite = asset_server.load(CROUCH_SPRITE);
+fn player_setup(mut commands : Commands, asset_server : Res<AssetServer>, ground_level : Res<GroundLevel>, mut texture_atlases: ResMut<Assets<TextureAtlas>>){
+    let idle_sprite = asset_server.load(IDLE_SPRITE);
+    let run_sprite = asset_server.load(RUN_SPRITE);
+    let texture_atlas_running = TextureAtlas::from_grid(
+        run_sprite,
+        Vec2::new(120.,80.),
+        10,
+        1,
+    );
+    let texture_atlas_handle_running = texture_atlases.add(texture_atlas_running);
+    let texture_atlas_idle = TextureAtlas::from_grid(
+        idle_sprite,
+        Vec2::new(120.,80.),
+        10,
+        1
+    );
+    let texture_atlas_handle_idle = texture_atlases.add(texture_atlas_idle);
     let level = ground_level.0;
-    commands.spawn_bundle(SpriteBundle{
-        texture : player_sprite,
+    commands.spawn_bundle(SpriteSheetBundle{
+        texture_atlas : texture_atlas_handle_running,
         transform : Transform {
-            translation : Vec3::new(0.,level,0.),
+            translation : Vec3::new(0.,level,1.),
             ..Default::default()
         },
         ..Default::default()
-    });
+    })
+    .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
 }
-*/
